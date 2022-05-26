665489bc77a3ccbbcd687c5b5913f1b280adf265
SergBobyr <serg_bobyr@ukr.net>
2022-05-26 20:26:26 +0300

    services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,9 @@
-import {Component} from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent{
-  e: number = Math.E
-  str= 'hello world'
-  date: Date = new Date()
-  float = 0.42
-  obj= {
-    a:1,
-  b:{
-      c:2,
-    d:{
-        e:3,
-      f:4
-    }
-  }
-}
+export class AppComponent  {
 }
