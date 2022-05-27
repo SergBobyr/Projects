ac32fd4b3d79628cc5fa084113bf9ab7151406a4
SergBobyr <serg_bobyr@ukr.net>
2022-05-27 22:01:16 +0300

    Отображение сообщений ошибок

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +10,22 @@ export class AppComponent implements OnInit{
   form: FormGroup
 
 ngOnInit(){
-    this.form = new FormGroup({})
+    this.form = new FormGroup({
+      email: new FormControl('', [
+        Validators.email, Validators.required]),
+      password: new FormControl(null,
+        [Validators.required,
+          Validators.minLength(6)])
+    })
   }
 
-  submit() {console.log('Form submitted:', this.form)
+  submit() {
+    if (this.form.valid) {
+      console.log('Form :', this.form)
+      const formData = {...this.form.value}
+      console.log('Form Data:', formData)
+    }
+
 
   }
 }
