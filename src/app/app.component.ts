93f0ce950af5db6a11e61d80741974b1141ae57b
SergBobyr <serg_bobyr@ukr.net>
2022-05-28 13:19:58 +0300

    Создание ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,13 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  form: FormGroup
-
-ngOnInit(){
-    this.form = new FormGroup({
-      email: new FormControl('', [
-        Validators.email, Validators.required]),
-      password: new FormControl(null,
-        [Validators.required,
-          Validators.minLength(6)])
-    })
-  }
-
-  submit() {
-    if (this.form.valid) {
-      console.log('Form :', this.form)
-      const formData = {...this.form.value}
-      console.log('Form Data:', formData)
-    }
-
-
+export class AppComponent {
+appState = 'off'
+  handleChange(){
+  console.log(this.appState)
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { SwitchComponent } from './switch/switch.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SwitchComponent,
   ],
   imports: [
     BrowserModule,
