5a410f3f82f93e6525b815c5f6996255b6c86fe8
SergBobyr <serg_bobyr@ukr.net>
2022-06-09 20:08:49 +0300

    Тестирование eventEmitter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CountreComponent} from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountreComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/counter/counter.component.spec.ts
@@ -0,0 +1,24 @@
+import {CounterComponent} from './counter.component';
+describe('CounterComponent', ()=>{
+  let component: CounterComponent
+  beforEach(() => {
+    component = new CounterComponent()
+  })
+  const component = new CounterComponent()
+  it('should increment counter by 1', ()=>{
+
+    component.increment()
+    expect(component.counter).toBe(1)
+  })
+  it('should decrement counter by 1', ()=>{
+
+    component.decrement()
+    expect(component.counter).toBe(-1)
+  })
+it('should increment value by event emitter', ()=>{
+let result = null
+  component.counterEmitter.subscribe(v => result = v)
+  component.increment()
+  expect(result).toBe(1)
+})
+})
