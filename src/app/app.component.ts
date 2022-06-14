ff28725df457d2f09c0fb635e76c4b5ecdb2452e
SergBobyr <serg_bobyr@ukr.net>
2022-06-14 19:56:06 +0300

    Сложные переходы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('box', [
+      state('start', style({ background: 'blue' })),
+      state('end', style({
+        background: 'red',
+        transform: 'scale(1.2)'
+      })),
+      state('special', style({
+background: 'orange',
+        transform: 'scale(0.5)',
+        borderRadius: '50%'
+      })),
+      transition('start => end', animate(450)),
+      transition('end => start', animate('800ms ease-in-out')),
+      transition('special<=>*',[
+        style({background: 'green'}),
+        animate('1s', style({
+          background: 'pink'
+        })),
+        animate(750)
+      ]),
+
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'angular-unit-tests';
+  boxState = 'start'
+
+  animate() {
+    this.boxState = this.boxState === 'end' ? 'start' : 'end'
+  }
 }
